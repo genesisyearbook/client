59dfe1b35229f99afa4a02a6c50caf827492415d
Syed Rehman <rehmomar1@gmail.com>
2019-08-26 12:57:31 -0400

    Added support for partials and made the navbar into a partial.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
-app.engine('handlebars', exphbs({
-    layoutsDir: path.join(__dirname, 'views/layouts')
-}));
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  exphbs({
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials")
+  })
+);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index", { layout: "main" });
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
