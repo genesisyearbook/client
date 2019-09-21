3529b3fb2c657e05222e977ca5b359df4014d0d4
James Johnson <me@jamesjohnson.io>
2019-09-21 12:16:58 -0400

    Made calendar its own page, due to size. Was able to parse information from about events:  - title  - start  - end Gonna have to figure out how to implement a way to change the color of the event when we have pictures of it.

--- src/app.js
+++ src/app.js
@@ -1,49 +1,54 @@
 const axios = require('axios');
-const path = require("path");
-const express = require("express");
-const exphbs = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 app.engine(
-  "handlebars",
-  exphbs({
-    layoutsDir: path.join(__dirname, "views/layouts"),
-    partialsDir: path.join(__dirname, "views/partials"),
-    helpers: {
-      ifEquals: (arg1, arg2, options) => {
-        return arg1 === arg2 ? options.fn(this) : options.inverse(this);
-      },
-      apiCall: (endpoint) => {
-        return axios.get(`${process.env.API}${endpoint}`)
-          .catch(e => console.error(e));
-      }
-    }
-  })
+    'handlebars',
+    exphbs({
+        layoutsDir: path.join(__dirname, 'views/layouts'),
+        partialsDir: path.join(__dirname, 'views/partials'),
+        helpers: {
+            ifEquals: (arg1, arg2, options) => {
+                return arg1 === arg2 ? options.fn(this) : options.inverse(this);
+            },
+            apiCall: endpoint => {
+                return axios
+                    .get(`${process.env.API}${endpoint}`)
+                    .catch(e => console.error(e));
+            }
+        }
+    })
 );
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "handlebars");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-  res.render("index", { layout: "main", activePage: "index" });
+app.get('/', (req, res) => {
+    res.render('index', { layout: 'main', activePage: 'index' });
 });
 
-app.get("/upload", (req, res) => {
-  res.render("upload", { layout: "main", activePage: "upload" });
+app.get('/upload', (req, res) => {
+    res.render('upload', { layout: 'main', activePage: 'upload' });
 });
 
-app.get("/gallery", (req, res) => {
-  res.render("gallery", { layout: "main", activePage: "gallery" });
+app.get('/calendar', (req, res) => {
+    res.render('calendar', { layout: 'main', activePage: 'calendar' });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", { layout: "main", activePage: "about" });
+app.get('/gallery', (req, res) => {
+    res.render('gallery', { layout: 'main', activePage: 'gallery' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { layout: 'main', activePage: 'about' });
 });
 
 const port = process.env.CLIENT_PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
